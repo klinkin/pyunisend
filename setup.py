0c9fcc50f877a1fb7e75471dad496171fe223c9b
dev <dev@devs.local>
2017-03-30 19:08:25 +0300

    Added setup file

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""
+PyUniSend is a simple API wrapper for interacting with http://UniSender.com
+-----------
+Links
+`````
+* `UniSender API documentation <http://www.unisender.com/en/help/api/?a=112233>`_
+* `PyUniSend source <http://github.com/klinkin/pyunisend>`_
+"""
+
+from setuptools import setup, find_packages
+import pyunisend
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: Russian',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+KEYWORDS = 'unisender api wrapper'
+
+setup(
+    name='pyunisend',
+    version=pyunisend.__version__,
+    author=pyunisend.__author__,
+    author_email=pyunisend.__author_email__,
+    license='MIT license',
+    description='API wrapper for interacting with http://UniSender.com',
+    long_description=__doc__,
+    url='https://github.com/klinkin/pyunisend',
+    packages=find_packages(),
+    download_url='http://pypi.python.org/pypi/pyunisend/',
+    classifiers=CLASSIFIERS,
+    keywords=KEYWORDS,
+    platforms='any',
+    zip_safe=True,
+    install_requires=[]
+)
